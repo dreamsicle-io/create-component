d5fd7de7ecf45ed4d29d7360fdafd7fdced7df7a
Noah Mason <noahmason23@gmail.com>
2022-05-21 17:43:45 -0400

    Adds nested tests.

---
+++ examples/components/_Template/utils/_Template.utils.ts
@@ -0,0 +1,12 @@
+
+/**
+ * This is a test `_Template` util class file.
+ * 
+ * @since _version Added on `_date`
+ */
+export class _TemplateUtils {
+	
+	log() {
+		console.log('Hello from _Template.utils.ts!');
+	}
+}
---
+++ examples/components/_Template/utils/_template-utils.ts
@@ -0,0 +1,8 @@
+/**
+ * This is a test `_Template` util functions file.
+ * 
+ * @since _version Added on `_date`
+ */
+export function util() {
+	console.log(`Hello from _template-utils.ts!`);
+}
