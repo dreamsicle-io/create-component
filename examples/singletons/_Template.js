bf02d0a74afa8466a01e8ad793649ac3d7cdc06d
Noah Mason <noah@dreamsicle.io>
2024-06-08 12:18:54 -0400

    Adds example files.

---
+++ examples/singletons/_Template.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+
+/**
+ * This is a test `_Template` class file.
+ * 
+ * @since _version Added on `_date`
+ */
+export class _Template {
+	
+	log() {
+		console.log('Hello from _Template.js!');
+	}
+}
